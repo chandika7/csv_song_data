d1b2b9f246ceaa2d31e0f90a686b023baadad72d
Chandika Niroshan <chandikaa@gmail.com>
2022-10-22 01:14:27 +0100

    Retrieve the details for the top ranked song for a particular day

--- songrankinfo.py
+++ songrankinfo.py
@@ -1,8 +1,17 @@
 import csv
+import matplotlib
+from matplotlib import pyplot
+import unittest
+import pandas
 
-with open("charts.csv") as file:
-    csv_reader = csv.reader(file)
+#with open("charts.csv","r") as file:
+csv_reader = pandas.read_csv('charts.csv')
+#csv_reader = pandas.read_csv('charts.csv',index_col='Song',
+#            parse_dates=['date'],
+#            header=0,
+#            names=['date','rank','Song', 'artist','last-week','peak-rank','weeks-on-board'])
 
+#print(csv_reader1)
 
 def main():
     menu()
@@ -41,7 +50,13 @@ def menu():
 
 
 def top_ranked_song():
-    print("Retrieve the details for the top ranked song for a particular day")
+#    print(csv_reader)
+    sub_listrank= csv_reader[csv_reader['rank'] ==1]
+    sub_listrank2=(sub_listrank.drop_duplicates(subset=['date']))
+    blankIndex=[''] * len(sub_listrank2)
+    sub_listrank2.index=blankIndex
+    print(sub_listrank2[['date','song']])
+#print(df[['name', 'score']])
     pass
 
 def top_ranked_song_artist():
@@ -58,8 +73,15 @@ def most_ranking_board():
 
 def Visualise_top_songs():
     print("Visualise the top songs")
+    data = (3, 6, 9, 12, 15)
+    fig, simple_chart = matplotlib.pyplot.subplots()
+    simple_chart.plot(data)
+    matplotlib.pyplot.show()
     pass
 
 
 # the program is initiated
-main()
+#main()
+top_ranked_song()
+#Visualise_top_songs()
+#unittest.main()
